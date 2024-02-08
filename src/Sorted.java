6e69d9c2458a6e01fe63d41f04774ed76f1222c4
WaleedBader <hurywaleed22@gmail.com>
2024-02-08 16:55:27 +0300

    selection sort

--- src/Sorted.java
+++ src/Sorted.java
@@ -1,5 +1,21 @@
 public class Sorted {
     public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minElementIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minElementIndex] > arr[j]) {
+                    minElementIndex = j;
+                }
+            }
 
+            if (minElementIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minElementIndex];
+                arr[minElementIndex] = temp;
+            }
+        }
     }
 }
